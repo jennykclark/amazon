f63f0869b75ba4b4a903880dd7e61e9aa38655a0
Jenny Clark <78026208+jennykclark@users.noreply.github.com>
2021-12-16 18:20:55 +0000

    Add files via upload

---
+++ email/buttomEmail.php
@@ -0,0 +1,98 @@
+<?php 
+$_title = 'Verify User';
+require_once(__DIR__.'../../components/header.php');
+session_start();
+if(! isset($_SESSION['user']['user_name'])){
+    header('Location: ../sign_in/sign_in.php');
+    die();
+}
+?>
+<body id="itemsBody">
+    <nav id="amazonNav">
+        <div id="logo">
+        <a href="../home/home.php"><img src="../image/amazonLogo.png" id="amazonLogo"></a>
+
+        </div>
+        <div id="Delivery">
+            <i class="fas fa-map-marker-alt"></i> 
+            <p><span id="deliveryGrey">Deliver to</span> <br> Denmark</p>
+        </div>
+        <div id="search">
+            <div id="dropdown"><p>All</p></div>
+            <input id="searchbar" onkeyup="search_animal()" type="text" name="search"> <button id="searchButton"><i class="fas fa-search"></i></button>
+        </div>
+        <div id="tools">
+        <div id="flag">
+            <img src="../image/denmark.png" id="denmarkFlag">
+        </div>
+        <div id="account" class="dropdown">
+           
+           <button class="dropbtn"><p>Hello 
+           <?php echo $_SESSION['user']['user_name'] ?>
+            <!-- Add php echo session user name -->
+            <!-- <span>Account &amp; Lists</span></p> -->
+            <span>Account</span></p>
+        </button> 
+        <div class="dropdown-content">
+            <h4>Account</h4>
+            <a href="../account/account.php">Your Account</a>
+            <a href="../add_items/upload_item.php">Add Items</a>
+            <a href="../update_items/basic_update.php">Update Items</a>
+            <a href="../sign_in/sign_in.php">Sign In</a>
+            <a href="../logout/logout.php">Sign out</a>
+            </div>
+
+        </div>
+        <div id="Order">
+            <!-- <p> Return </p>
+             <p>&amp;Orders</p> -->
+             <p>Return
+                 <span>&amp;Orders</span>
+             </p>
+        </div>
+        <div id="basket">
+            <!-- <p>Basket</p> -->
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+        </div>
+    </nav>
+<div id="secondnav">
+    <div id="all">
+        <p> &#9776; All</p>
+    </div>
+    <div id="deals">
+        <p>Last Mintue Deals</p>
+    </div>
+    <div id="grocery">
+        <p>Grocery</p>
+    </div>
+    <div id="free">
+        <p>Free Delivery</p>
+    </div>
+    <div id="buy">
+        <p>Buy Again</p>
+    </div>
+</div>
+
+    
+<div id="email">
+    <section id="signUP">
+    <section id="smallersignin">
+        <div id="sign">
+        <h3>
+        Please Validate your email address, so you can use our site.
+    </h3>
+<button id="uploadButton" onclick="send_email()">Send email</button>
+
+        </div>
+  
+    </section>
+
+</section>
+</div>
+
+
+
+<?php
+require_once(__DIR__.'../../components/footer.php');
+?>
